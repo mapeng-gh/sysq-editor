8a5f83c863b984be6f23f7183b1bace982a8b9b8
mapeng <mapeng_wy@163.com>
2018-11-14 14:13:27 +0800

    录音下载：ftp服务器改为内网地址

--- src/com/huasheng/sysq/editor/controller/CommonController.java
+++ src/com/huasheng/sysq/editor/controller/CommonController.java
@@ -1,7 +1,6 @@
 package com.huasheng.sysq.editor.controller;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,7 +21,7 @@ import com.huasheng.sysq.editor.util.LogUtils;
 @RequestMapping(value="/common")
 public class CommonController {
 	
-	private static final String FTP_HOST = "ccpl.psych.ac.cn";
+	private static final String FTP_HOST = "192.168.8.1";//ccpl.psych.ac.cn 192.168.8.1
 	private static final int FTP_PORT = 20020;
 	private static final String FTP_USER = "anonymous";
 	private static final String FTP_PASSWD = "";
@@ -34,57 +33,69 @@ public class CommonController {
 	 * @throws IOException 
 	 */
 	@RequestMapping(value="/downloadAudio.do",method=RequestMethod.GET)
-	public void downloadAudio(@RequestParam(value="interviewId") int interviewId , HttpServletResponse response) throws IOException {
+	public void downloadAudio(@RequestParam(value="interviewId") int interviewId , HttpServletResponse response) {
 		LogUtils.info(this.getClass(), "downloadAuto params : interviewId = {}",interviewId);
 		
-		//下载设置
-		response.setHeader("Content-Disposition","attachment; filename=" + interviewId + ".zip");
-		OutputStream os = response.getOutputStream();
-		
-		//FTP设置
-		FTPClient ftpClient = new FTPClient();
-		ftpClient.connect(FTP_HOST,FTP_PORT);
-		ftpClient.login(FTP_USER, FTP_PASSWD);
-		ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-		ftpClient.enterLocalPassiveMode();
-		ftpClient.changeWorkingDirectory(FTP_WORKING_DIR);
-		LogUtils.info(this.getClass(), "downloadAuto[{}] : FTP settings ok",interviewId);
-		
-		//文件过滤
-		FTPFile files[] = ftpClient.listFiles("",new FTPFileFilter() {
-			@Override
-			public boolean accept(FTPFile file) {
-				if(file.isFile() && file.getName().startsWith(interviewId + "")) {
-					return true;
+		FTPClient ftpClient = null;
+		ZipArchiveOutputStream zos = null;
+		try {
+			//下载设置
+			response.setHeader("Content-Disposition","attachment; filename=" + interviewId + ".zip");
+			
+			//FTP设置
+			ftpClient = new FTPClient();
+			ftpClient.connect(FTP_HOST,FTP_PORT);
+			ftpClient.login(FTP_USER, FTP_PASSWD);
+			ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+			ftpClient.enterLocalPassiveMode();
+			ftpClient.changeWorkingDirectory(FTP_WORKING_DIR);
+			LogUtils.info(this.getClass(), "downloadAuto[{}] : FTP settings ok",interviewId);
+			
+			//文件过滤
+			FTPFile files[] = ftpClient.listFiles("",new FTPFileFilter() {
+				@Override
+				public boolean accept(FTPFile file) {
+					if(file.isFile() && file.getName().startsWith(interviewId + "")) {
+						return true;
+					}
+					return false;
 				}
-				return false;
+			});
+			if(files == null || files.length == 0) {
+				LogUtils.info(this.getClass(), "downloadAuto[{}] : no audio file",interviewId);
+				response.getWriter().write("没有查询到该访谈对应的录音文件");
+				return;
 			}
-		});
-		if(files == null || files.length == 0) {
-			LogUtils.info(this.getClass(), "downloadAuto[{}] : no audio file",interviewId);
-			os.write(new String("没有查询到该访谈对应的录音文件").getBytes("UTF-8"));
-			ftpClient.logout();
-			ftpClient.disconnect();
-			os.close();
-			return;
+			
+			//压缩输出
+			zos = new ZipArchiveOutputStream(response.getOutputStream());
+			zos.setEncoding("UTF-8");
+			for(FTPFile ftpFile : files) {
+				ZipArchiveEntry zipEntry = new ZipArchiveEntry(ftpFile.getName());
+				zos.putArchiveEntry(zipEntry);
+				ftpClient.retrieveFile(ftpFile.getName(), zos);
+				zos.closeArchiveEntry();
+				LogUtils.info(this.getClass(), "downloadAuto[{}] : file download finished = {}",interviewId,ftpFile.getName());
+			}
+			zos.finish();
+			LogUtils.info(this.getClass(), "downloadAuto[{}] : finished",interviewId);
+			
+		}catch(Exception e) {
+			LogUtils.error(this.getClass(), "downloadAuto["+interviewId+"] error", e);
+			try {
+				response.getWriter().write("下载录音文件失败");
+			}catch(Exception e1) {}
+			
+		}finally {
+			try {
+				if(zos != null) {
+					zos.close();
+				}
+				if(ftpClient != null) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+				}
+			}catch(Exception e) {}
 		}
-		
-		//压缩输出
-		ZipArchiveOutputStream zos = new ZipArchiveOutputStream(os);
-		zos.setEncoding("UTF-8");
-		for(FTPFile ftpFile : files) {
-			ZipArchiveEntry zipEntry = new ZipArchiveEntry(ftpFile.getName());
-			zos.putArchiveEntry(zipEntry);
-			ftpClient.retrieveFile(ftpFile.getName(), zos);
-			zos.closeArchiveEntry();
-			LogUtils.info(this.getClass(), "downloadAuto[{}] : file download finished = {}",interviewId,ftpFile.getName());
-		}
-		
-		//资源关闭
-		zos.finish();
-		zos.close();
-		ftpClient.logout();
-		ftpClient.disconnect();
-		LogUtils.info(this.getClass(), "downloadAuto[{}] : finished",interviewId);
 	}
 }
